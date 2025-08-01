c80793c98776992074f00826b754d939f50eeb37
Carlos Ayaviri <carlos.ashax.11@gmail.com>
2025-08-01 15:58:46 -0400

    Ejercicio 4 finalizado, servidor y prueba en postman correcta

---
+++ Ejercicio4.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, jsonify
+import mysql.connector
+
+app = Flask(__name__)
+
+def conectar():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Mandrake99",
+        database="clientes_bayma"
+    )
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.get_json()
+
+    if 'evento' not in data or 'mensaje' not in data or 'timestamp' not in data:
+        return jsonify({'status': 'error', 'mensaje': 'Faltan campos'}), 400
+
+
+    try:
+        connection = conectar()
+        cursor = connection.cursor()
+
+        query = "INSERT INTO logs (evento, mensaje, timestamp) VALUES (%s, %s, %s)"
+        cursor.execute(query, (data['evento'], data['mensaje'], data['timestamp']))
+        connection.commit()
+
+        cursor.close()
+        connection.close()
+
+        return jsonify({
+            'status': 'ok',
+            'recibido': data
+        })
+
+    except Exception as e:
+        return jsonify({'status': 'error', 'mensaje': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
